77b18fe827a84164f626e3981e24aba95a151140
Matei <mateisr@proton.me>
2022-11-27 22:13:05 +0200

    Update prefix commands

---
+++ src/commands/music/pause.p.ts
@@ -0,0 +1,29 @@
+import { ApplicationCommandOptionType, Colors, Guild } from "discord.js";
+import { PrefixCommand } from '../../structures/PrefixCommand';
+
+export default new PrefixCommand({
+    name: "pause",
+    description: "Pause playing song",
+    run: async ({ client, message, args }) => {
+
+        // Check if user is in a voice channel
+        if (!message.member.voice.channelId) return await message.reply({ embeds: [client.util.embed("You are not in a voice channel", Colors.Red, "Please join a voice channel and try again")] });
+
+        const dispatcher = await client.manager.get(message.guild.id);
+        if (!dispatcher || !dispatcher.current) return await message.reply({ embeds: [client.util.embed("No song is playing", Colors.Red, "Please play a song first")] });
+
+        // Check if user is in the same voice channel as the bot
+        if (message.member.voice.channelId !== dispatcher.voiceChannelId) return await message.reply({ embeds: [client.util.embed("You are not in my voice channel", Colors.Red, "Please join my voice channel and try again")] });
+
+        // Check if song is already paused - resume it
+        if (dispatcher.player.paused) {
+            dispatcher.resume();
+            return await message.reply({ embeds: [client.util.embed("Resumed", Colors.Green, `Resumed playing [${dispatcher.current.info.title}](${dispatcher.current.info.uri})`)] });
+        }
+        else {
+            // Pause the player
+            dispatcher.pause();
+            return await message.reply({ embeds: [client.util.embed("Paused", Colors.Green, `Paused playing [${dispatcher.current.info.title}](${dispatcher.current.info.uri})`)] });
+        }
+
+    }});
---
+++ src/commands/music/queue.p.ts
@@ -0,0 +1,49 @@
+import { Guild, Colors, EmbedBuilder, GuildMember, ApplicationCommandOptionType } from 'discord.js';
+import { PrefixCommand } from "../../structures/PrefixCommand";
+
+export default new PrefixCommand({
+    name: "queue",
+    description: "Shows the music queue for the current server",
+    async run({ message, args, client }) {
+        // fetch bot guild member
+        const guild = message.guild as Guild;
+        const bot = await guild.members.fetch(client.user.id);
+        // check if member is in a voice channel
+        if (!message.member.voice.channelId) return await message.reply({ embeds: [client.util.embed("You are not in a voice channel", Colors.Red, "Please join a voice channel and try again")] });
+        // check if bot is in the same voice channel as the user
+        if (bot.voice.channelId && message.member.voice.channelId !== bot.voice.channelId) return await message.reply({ embeds: [client.util.embed("You are not in my voice channel", Colors.Red, "Please join my voice channel and try again")] });
+
+        const dispatcher = await client.manager.get(message.guild.id);
+
+        if (!dispatcher) return await message.reply({ embeds: [client.util.embed("No music is playing", Colors.Red, "Please play a song and try again")] });
+
+        if (dispatcher.queue.length === 0) return await message.reply({ embeds: [client.util.embed("No songs in the queue", Colors.Red, "There are no songs left in the queue, add a song and try again")] });
+
+        const queriedPage = parseInt(args[0])|| 1;
+        const maxPages = Math.ceil(dispatcher.queue.length / 10);
+        const selectedPage = (queriedPage > maxPages || queriedPage < 1) ? maxPages : queriedPage;
+
+        try {
+            const queue = dispatcher.queue.map((t, i) => {
+                const member = message.guild.members.cache.get(t.info.author) as GuildMember;
+                return `\`${++i}.\`[${t.info.title}](${t.info.uri}) ${t.info.isStream ? '**Stream' : `- **${client.util.formatTime(t.info.length)}** [${member}]`} `});
+            const chunked = client.util.chunk(queue, 10);
+            const embeds = new Array<EmbedBuilder>();
+
+            var page = 0;
+            for (const chunk of chunked) {
+                page++;
+                const embed = client.util.embed(`Music Queue - ${guild.name}`, Colors.Purple, `${chunk.join('\n')}`).setFooter({"text": `Page ${page}/${chunked.length}`});
+                embeds.push(embed);
+            }
+
+            // send message for selected page
+            const msg = await message.reply({ embeds: [embeds[selectedPage - 1]] });
+
+        }  catch (error) {
+            console.error(error);
+            // Send embed response
+            return await message.reply({ embeds: [client.util.embed("An error occurred while trying to run the command", Colors.Red, `Please try again later\n\`\`\`${error}\`\`\``)] });
+        }
+
+    }});
