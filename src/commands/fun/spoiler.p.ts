35ba2aeba19d6f2a7f16e89c2f9d95a8ca3557db
Matei <mateisr@proton.me>
2022-11-27 22:43:10 +0200

    Spoiler commands

---
+++ src/commands/fun/spoiler.p.ts
@@ -0,0 +1,14 @@
+import { PrefixCommand } from "../../structures/PrefixCommand";
+
+export default new PrefixCommand({
+    name: "spoiler",
+    description: "spoiler your text",
+    async run({ message, args, client }) {
+
+        if (!args.length) return;
+        let reply = args.join(" ");
+        // spoiler every character
+        reply = reply.split("").map(c => `||${c}||`).join("");
+        return await message.reply(reply);
+
+    }});
