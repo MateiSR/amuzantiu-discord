9cd625903e276348dcc38709fc90dd1d310acfbe
Matei <mateisr@proton.me>
2022-11-27 16:53:23 +0200

    Prefix command "purge" changes
    Check for valid amount, number amount

--- src/commands/utils/purge.p.ts
+++ src/commands/utils/purge.p.ts
@@ -8,6 +8,10 @@ export default new PrefixCommand({
     run: async ({ client, message, args }) => {
 
         if (!args[0]) return;
+        if (isNaN(parseInt(args[0]))) return;
+        // Check if amount is between 1 and 100
+        if (parseInt(args[0]) < 1 || parseInt(args[0]) > 100) return await message.reply("Please enter a number between 1 and 100");
+
         const amount: number = parseInt(args[0]);
         const messages = await message.channel.messages.fetch({
             limit: amount
