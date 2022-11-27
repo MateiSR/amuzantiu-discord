80fa17bf30c0e9645de272ab39f1c983308f667b
Matei <mateisr@proton.me>
2022-11-27 16:53:05 +0200

    Add pause, resume & stop

---
+++ src/commands/music/pause.ts
@@ -0,0 +1,29 @@
+import { ApplicationCommandOptionType, Colors, Guild } from "discord.js";
+import { Command } from "../../structures/Command";
+
+export default new Command({
+    name: "pause",
+    description: "Pause playing song",
+    run: async ({ client, interaction, args }) => {
+
+        // Check if user is in a voice channel
+        if (!interaction.member.voice.channelId) return await interaction.followUp({ embeds: [client.util.embed("You are not in a voice channel", Colors.Red, "Please join a voice channel and try again")] });
+
+        const dispatcher = await client.manager.get(interaction.guild.id);
+        if (!dispatcher || !dispatcher.current) return await interaction.followUp({ embeds: [client.util.embed("No song is playing", Colors.Red, "Please play a song first")] });
+
+        // Check if user is in the same voice channel as the bot
+        if (interaction.member.voice.channelId !== dispatcher.voiceChannelId) return await interaction.followUp({ embeds: [client.util.embed("You are not in my voice channel", Colors.Red, "Please join my voice channel and try again")] });
+
+        // Check if song is already paused - resume it
+        if (dispatcher.player.paused) {
+            dispatcher.resume();
+            return await interaction.followUp({ embeds: [client.util.embed("Resumed", Colors.Green, `Resumed playing [${dispatcher.current.info.title}](${dispatcher.current.info.uri})`)] });
+        }
+        else {
+            // Pause the player
+            dispatcher.pause();
+            return await interaction.followUp({ embeds: [client.util.embed("Paused", Colors.Green, `Paused playing [${dispatcher.current.info.title}](${dispatcher.current.info.uri})`)] });
+        }
+
+    }});
---
+++ src/commands/music/stop.ts
@@ -0,0 +1,27 @@
+import { ApplicationCommandOptionType, Colors, Guild } from "discord.js";
+import { Command } from "../../structures/Command";
+
+export default new Command({
+    name: "stop",
+    description: "stop playing song & clear queue",
+    run: async ({ client, interaction, args }) => {
+
+        // Check if user is in a voice channel
+        if (!interaction.member.voice.channelId) return await interaction.followUp({ embeds: [client.util.embed("You are not in a voice channel", Colors.Red, "Please join a voice channel and try again")] });
+
+        const dispatcher = await client.manager.get(interaction.guild.id);
+        if (!dispatcher || !dispatcher.current) return await interaction.followUp({ embeds: [client.util.embed("No song is playing", Colors.Red, "Please play a song first")] });
+
+        // Check if user is in the same voice channel as the bot
+        if (interaction.member.voice.channelId !== dispatcher.voiceChannelId) return await interaction.followUp({ embeds: [client.util.embed("You are not in my voice channel", Colors.Red, "Please join my voice channel and try again")] });
+
+        // Stop the player
+        try {
+        dispatcher.stop();
+        return await interaction.followUp({ embeds: [client.util.embed("Stopped", Colors.Green, `Stopped playing [${dispatcher.current.info.title}](${dispatcher.current.info.uri})`)] });
+        } catch (error) {
+            // Inform user of occured error
+            return await interaction.followUp({ embeds: [client.util.embed("Error", Colors.Red, `An error occured while trying to stop the player`)] });
+        }
+
+    }});
