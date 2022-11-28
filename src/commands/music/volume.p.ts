5a37485f919625296d5d953a5c83e52e6118df01
Matei <mateisr@proton.me>
2022-11-28 18:53:25 +0200

    Volume command & previous command aliases

--- src/commands/music/volume.p.ts
+++ src/commands/music/volume.p.ts
@@ -1,26 +1,32 @@
-import { Colors } from "discord.js";
+import { ApplicationCommandOptionType, Colors } from "discord.js";
 import { PrefixCommand } from '../../structures/PrefixCommand';
 
 
 export default new PrefixCommand({
-    name: "previous",
-    description: "Play previous song",
-    aliases: ["prev", "back"],
+    name: "volume",
+    description: "Sets the music volume",
+    cooldown: 3000,
     run: async ({ client, message, args }) => {
 
+        // Get dispatcher
         const dispatcher = await client.manager.get(message.guild.id);
+
         // Check if dispatcher exists
         if (!dispatcher || !dispatcher.current) return await message.reply({ embeds: [client.util.embed("No song is playing", Colors.Red, "Please play a song first")] });
-        // Check if there is a previous song
-        if (!dispatcher.previous) return await message.reply({ embeds: [client.util.embed("No previous song", Colors.Red, "There is no previous song to play")] });
+        // Check if plaing
+        if (dispatcher.player.paused) return await message.reply({ embeds: [client.util.embed("Music is paused", Colors.Red, "Please resume the music and try again")] });
+
         // Check if user is in a voice channel
         if (!message.member.voice.channelId) return await message.reply({ embeds: [client.util.embed("You are not in a voice channel", Colors.Red, "Please join a voice channel and try again")] });
         // Check if user is in the same voice channel as the bot
         if (message.member.voice.channelId !== dispatcher.voiceChannelId) return await message.reply({ embeds: [client.util.embed("You are not in my voice channel", Colors.Red, "Please join my voice channel and try again")] });
-        // Send success message
-        await message.reply({ embeds: [client.util.embed("Playing previous song", Colors.Green, `Playing **[${dispatcher.previous.info.title}](${dispatcher.previous.info.uri})**`)] });
-        // Play previous song
-        dispatcher.playPrevious();
-        return;
 
+        // Check if valid volume
+        if (!args[0] ||  parseInt(args[0]) < 0 || parseInt(args[0]) > 500) return await message.reply({ embeds: [client.util.embed("Invalid volume", Colors.Red, "Please enter a valid volume **between 1-500**")] });
+        // Get volume
+        const volume = parseInt(args[0]) || 100;
+        // Set volume
+        dispatcher.player.setVolume(volume / 100);
+        // Send success message
+        await message.reply({ embeds: [client.util.embed("Volume set", Colors.Green, `Set volume to **${volume}%**`)] });
 }});
---
+++ src/commands/music/volume.ts
@@ -0,0 +1,38 @@
+import { ApplicationCommandOptionType, Colors } from "discord.js";
+import { Command } from "../../structures/Command";
+
+
+export default new Command({
+    name: "volume",
+    description: "Sets the music volume",
+    options: [{
+        name: "volume",
+        description: "Volume to set",
+        type: ApplicationCommandOptionType.Integer,
+        required: true,
+        minValue: 0,
+        maxValue: 500
+    }],
+    cooldown: 3000,
+    run: async ({ client, interaction, args }) => {
+
+        // Get dispatcher
+        const dispatcher = await client.manager.get(interaction.guild.id);
+
+        // Check if dispatcher exists
+        if (!dispatcher || !dispatcher.current) return await interaction.followUp({ embeds: [client.util.embed("No song is playing", Colors.Red, "Please play a song first")] });
+        // Check if plaing
+        if (dispatcher.player.paused) return await interaction.followUp({ embeds: [client.util.embed("Music is paused", Colors.Red, "Please resume the music and try again")] });
+
+        // Check if user is in a voice channel
+        if (!interaction.member.voice.channelId) return await interaction.followUp({ embeds: [client.util.embed("You are not in a voice channel", Colors.Red, "Please join a voice channel and try again")] });
+        // Check if user is in the same voice channel as the bot
+        if (interaction.member.voice.channelId !== dispatcher.voiceChannelId) return await interaction.followUp({ embeds: [client.util.embed("You are not in my voice channel", Colors.Red, "Please join my voice channel and try again")] });
+
+        // Get volume
+        const volume = args.getInteger("volume", true);
+        // Set volume
+        dispatcher.player.setVolume(volume / 100);
+        // Send success message
+        await interaction.followUp({ embeds: [client.util.embed("Volume set", Colors.Green, `Set volume to **${volume}%**`)] });
+}});
